79261dc8431f675d7241dc5c25f912e177a89c36
yubYern <mynameisnak@gmail.com>
2024-03-30 22:41:41 +0700

    finished pytest test_lambda_api

---
+++ tests/test_lambda_api_ok.py
@@ -0,0 +1,18 @@
+#%%
+import requests
+import pytest
+
+URL = "https://qhq9zgl0nh.execute-api.us-east-1.amazonaws.com"
+ENDPOINT = '/write_to_dyndb'
+
+#%%
+
+def test_api_ok():
+    response = requests.get(URL+ENDPOINT)
+    assert response.status_code == 200
+    json_response = response.json()
+    before_update_view =  json_response['before_update']['Item']['views']
+    after_update_view =  json_response['after_update']['Item']['views']
+    assert after_update_view == before_update_view+1
+
+# %%
