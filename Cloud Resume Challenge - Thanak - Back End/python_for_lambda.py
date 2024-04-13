9c2d4f36d83ded9c8993bf15505f763ec75543b2
yubYern <mynameisnak@gmail.com>
2024-04-13 22:32:01 +0700

    Add Terraform iampolicy iamrole and update lambda function code

--- Cloud Resume Challenge - Thanak - Back End/python_for_lambda.py
+++ Cloud Resume Challenge - Thanak - Back End/python_for_lambda.py
@@ -1,16 +1,34 @@
+import json
 import boto3
-TABLE_NAME = 'cloud_resume_challenge_db'
+from datetime import datetime
+
+TABLE_NAME = 'de_job_post'
 dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(TABLE_NAME)
+
+now = str(datetime.now())
+print(f'Current Server Time = {now}')
 
 def lambda_handler(event, context):
-    table = dynamodb.Table(TABLE_NAME)
-
-    response = table.put_item(
-        Item={
-            'id': 1,
-            'ts': 'time_stamp_placeholder'
-        }
-    )
     
-    status_code = response['ResponseMetadata']['HTTPStatusCode']
-    print(status_code)
+    exist = table.get_item(Key={
+                    'id':1
+                })
+    last_view = exist['Item']['views']
+    current_view_count = last_view+1
+    
+    response = table.put_item(Item={
+            'id':1,
+            'ts': now,
+            'views': current_view_count
+        })
+        
+    current_table = table.get_item(Key={
+                    'id':1
+                })    
+    
+    return_pack = {'before_update':exist,
+                    'after_update': current_table
+                    }
+            
+    return return_pack
--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 provider "aws" {
   shared_config_files = [var.tfc_aws_dynamic_credentials.default.shared_config_file]
-  region = var.region
+  region              = var.region
 }
 
+#import existing DynamoDB to preserve content to the new one TF create
 import {
   to = aws_dynamodb_table.cloud_resume_challenge_db
   id = "arn:aws:dynamodb:us-east-1:861150920151:table/cloud_resume_challenge_db"
@@ -23,5 +24,62 @@ resource "aws_dynamodb_table" "cloud_resume_challenge_db" {
   tags = {
     Name = "Cloud Resume Challenge"
   }
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "terraform_lambda_func_role"
+  assume_role_policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Action" : "sts:AssumeRole",
+          "Principal" : {
+            "Service" : "lambda.amazonaws.com"
+          },
+          "Effect" : "Allow",
+          "Sid" : ""
+        }
+      ]
+    }
+  )
+}
+
+resource "aws_iam_policy" "iam_policy_for_lambda" {
+  name        = "aws_iam_policy_for_terraform_lambda_func_role"
+  path        = "/"
+  description = "AWS IAM Policy for managing aws lambda role"
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
+          ],
+          "Resource" : "arn:aws:logs:*:*:*",
+
+        },
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "dynamodb:UpdateItem",
+            "dynamodb:GetItem",
+            "dynamodb:PutItem"
+          ],
+          "Resource" : aws_dynamodb_table.cloud_resume_challenge_db.arn
+        },
+      ]
+    }
+  )
+}
+
+## attach policy to newly created role
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.iam_policy_for_lambda.arn
 }
 
