743a9a4f0cd5e65af88320e927c53c2aa5ee9020
yubYern <mynameisnak@gmail.com>
2024-04-14 00:22:17 +0700

    first commit for full terraform resources

--- main.tf
+++ main.tf
@@ -83,3 +83,96 @@ resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
   policy_arn = aws_iam_policy.iam_policy_for_lambda.arn
 }
 
+#define
+data "archive_file" "zip_python_code" {
+  type        = "zip"
+  source_dir  = "${path.module}/lambda_function"
+  output_path = "${path.module}/lambda_function/lambda_function.zip"
+}
+
+#construct lambda function
+resource "aws_lambda_function" "terraform_lambda_write_to_dyndb" {
+  filename      = data.archive_file.zip_python_code.output_path
+  function_name = "terraform_lambda_write_to_dyndb"
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.8"
+  depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+  environment {
+    variables = {
+      databaseName = aws_dynamodb_table.cloud_resume_challenge_db.name
+    }
+  }
+}
+
+#create Cloudwatch Log group and API Gateway
+
+resource "aws_cloudwatch_log_group" "api_gateway" {
+  name              = "terraform_visitor_count_log_group"
+  retention_in_days = 30
+}
+
+resource "aws_apigatewayv2_api" "api_gateway_for_lambda" {
+  name          = "terraform_visitor_count_api_gateway"
+  protocol_type = "HTTP"
+  description   = "terraform API Gateway to trigger Lambda function to update visitor count"
+  cors_configuration {
+    allow_origins = ["https://www.thanak.net"]
+    allow_methods = ["GET"]
+  }
+}
+
+
+#set up stage of API Gateway to "Default" with enabling auto-deployment and config access log
+resource "aws_apigatewayv2_stage" "default_api_stage" {
+  api_id      = aws_apigatewayv2_api.api_gateway_for_lambda.id
+  name        = "default"
+  auto_deploy = true
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api_gateway.arn
+    format = jsonencode({
+      requestId               = "$context.requestID"
+      sourceIp                = "$context.identity.sourceIp"
+      requestTime             = "$context.identity.requestTime"
+      protocol                = "$context.protocol"
+      httpMethod              = "$context.httpMethod"
+      resourcePath            = "$context.resourcePath"
+      routeKey                = "$context.routeKey"
+      status                  = "$context.status"
+      responseLength          = "$context.responseLength"
+      integrationErrorMessage = "$context.integrationErrorMessage"
+    })
+  }
+  tags = {
+    Name = "Cloud Resume Challenge"
+  }
+}
+
+#Connect API Gateway with Lambda Function
+resource "aws_apigatewayv2_integration" "integrated_lambda_function" {
+  api_id             = aws_apigatewayv2_api.api_gateway_for_lambda.id
+  integration_uri    = aws_lambda_function.terraform_lambda_write_to_dyndb.invoke_arn
+  integration_type   = "AWS_PROXY"
+  integration_method = "GET"
+}
+
+#construct API Route
+resource "aws_apigatewayv2_route" "terraform_lambda_api_gw_route" {
+  api_id    = aws_apigatewayv2_integration.integrated_lambda_function.id
+  route_key = "ANY /terraform_lambda_write_to_dyndb"
+  target    = "integration/${aws_apigatewayv2_integration.integrated_lambda_function.id}"
+}
+
+# Add permission for gateway to access lambda function
+resource "aws_lambda_permission" "api_gateway_permission" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.terraform_lambda_write_to_dyndb.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.api_gateway_for_lambda.execution_arn}/*/*"
+}
+
+
+
+
+
