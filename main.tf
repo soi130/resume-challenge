225121a62a81be26a1b4197ac37e175c6181720f
yubYern <mynameisnak@gmail.com>
2024-04-16 22:23:38 +0700

    add functionhash to lambda

--- main.tf
+++ main.tf
@@ -97,6 +97,7 @@ resource "aws_lambda_function" "terraform_lambda_write_to_dyndb" {
   function_name = "terraform_lambda_write_to_dyndb"
   role          = aws_iam_role.lambda_role.arn
   handler       = "lambda_function.lambda_handler"
+  source_code_hash = data.archive_file.zip_python_code.output_base64sha256  
   runtime       = "python3.8"
   depends_on    = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
   environment {
