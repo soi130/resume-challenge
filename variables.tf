be210af0549101a82677161e13cca576f4c459ae
yubYern <mynameisnak@gmail.com>
2024-04-19 00:05:11 +0700

    test variables

---
+++ variables.tf
@@ -0,0 +1,51 @@
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "tfc_aws_dynamic_credentials" {
+  description = "Object containing AWS dynamic credentials configuration"
+  type = object({
+    default = object({
+      shared_config_file = string
+    })
+    aliases = map(object({
+      shared_config_file = string
+    }))
+  })
+}
+
+variable "s3_bucket_name" {
+  description = "Terraform S3 Name for static web hosting"
+  default     = "terraform-thanak.net"
+}
+
+variable "s3_bucket_policy" {
+  description = "S3 public access policy"
+  default = {
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "PublicReadGetObject",
+        "Effect" : "Allow",
+        "Principal" : "*",
+        "Action" : "s3:GetObject",
+        "Resource" : "arn:aws:s3:::terraform-thanak.net/*"
+      }
+    ]
+  }
+}
+
+variable "original_file_paths_for_s3" {
+  default = [
+    "Cloud Resume Challenge - Thanak - Front End/index.html",
+    "Cloud Resume Challenge - Thanak - Front End/index.js",
+    "Cloud Resume Challenge - Thanak - Front End/resume.css"
+  ]
+}
+
+variable "project_tag" {
+  default = "Cloud Resume Challenge"
+}
+
+#test
