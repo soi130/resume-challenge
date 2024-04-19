2b215609462dcea2e87a41cba95780e218b91283
yubYern <mynameisnak@gmail.com>
2024-04-19 09:45:30 +0700

    add ssl support method on CF distribution

--- frontend.tf
+++ frontend.tf
@@ -154,6 +154,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
   viewer_certificate {
     acm_certificate_arn = aws_acm_certificate.ssl_cert.arn
+    ssl_support_method = "sni-only"
   }
 }
 
